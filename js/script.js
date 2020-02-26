c9b9f73c26c83a8daac060f6db018f89f7da9d6d
Dan094 <Dhanikamarasekera@gmail.com>
2020-02-25 20:03:34 -0800

    Several changes
    add left side About us window, minor new section changes,  added new services section

--- js/script.js
+++ js/script.js
@@ -3,3 +3,36 @@
   $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
+      
+
+  var logo = document.querySelector('.logo');
+  var aboutus = document.querySelector('.aboutus');
+  var aboutusEmpt = document.querySelector('.aboutusEmpty');
+
+
+aboutusEmpt.addEventListener('click', function (){
+  aboutus.style.marginLeft=-200+"rem";
+  aboutusEmpty.style.opacity="1";
+});
+
+aboutus.addEventListener('click', function (){
+  aboutus.style.marginLeft=-200+"rem";
+  aboutusEmpty.style.opacity="1";
+});
+
+logo .addEventListener('click', function (){
+  aboutus.style.marginLeft=0+"rem";
+  aboutusEmpty.style.opacity="0";
+});
+  
