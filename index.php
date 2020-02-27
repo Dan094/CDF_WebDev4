47a09840040172bb5215dfe638a3ae22af1266ef
Dan094 <Dhanikamarasekera@gmail.com>
2020-02-27 07:37:15 -0800

    Minor changes and added pallarax library

--- index.php
+++ index.php
@@ -19,19 +19,20 @@
                  <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
                  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
                  <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+                
         </head>
 
   <body style="overflow-x: hidden;">
-        <nav style="position:fixed; z-index:2; height:7rem; padding:1rem;" class="navColor" id="nav">
+        <nav style="position:fixed; z-index:3; height:7rem; padding:1rem;" class="navColor" id="nav">
                 <div class="nav-wrapper">
                         <a href="#" class="brand-logo logo">
                                 <img src="./images/logo.png" width="200px" style="position: absolute; z-index: 100;" >
                         </a>
                         <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
                         <ul class="right hide-on-med-and-down">
-                        <li><a href="sass.html">Home</a></li>
-                        <li><a href="Index.html#section5">Services</a></li>
-                        <li><a href="Index.html#section3">News</a></li>
+                        <li><a href="">Home</a></li>
+                        <li><a href="Index.php#section5">Services</a></li>
+                        <li><a href="Index.php#section3">News</a></li>
                         <li><a href="collapsible.html">Contact</a></li>
                         <li><a  class="waves-effect waves-light btn-large waves-effect waves-light btn modal-trigger" data-target="modal1" class="btn modal-trigger" id="loginBtn" style="background-color: rgb(181, 211, 61);font-weight:bold;">Login</a></li>
                         </ul>
@@ -82,12 +83,12 @@
                         <div class=" heroimg" >
                                 <div class="row">
                                         <div class="col xl5 l5 m8 s10"  style="margin-top:15rem; z-index:-20;">
-                                                <h1 class="bigtxt" data-aos="fade-up">Your adventure of a lifetime awaits!</h1>
+                                                 <h1 class="bigtxt lax" data-lax-translate-y="0 0, 400 -400" data-lax-opacity="0 1, 100 0">Your adventure of a lifetime awaits!</h1>
                                         </div>
                                         <div class="col l10 m5">
-                                                <a class="waves-effect waves-light btn-large" style="background-color: #F3A530;font-weight:bold;">Donate</a>
+                                                <a class="lax waves-effect waves-light btn-large" style="background-color: #F3A530;font-weight:bold;" data-aos="fade-in" data-lax-opacity="0 1, 100 0">Donate</a>
                                         </div>
-                                        <div class="col l10 row scrollsection" style="width:100%; position: relative;">
+                                        <div class="col l10 row scrollsection  lax" style="width:100%; position: relative;" data-lax-translate-y="0, 0, 400, -400" >
                                                <a href="#section3" class="scrollbox" >
                                                         <h5>Scroll Down</h5>
                                                         <img src="images/scroll_btn.svg" width="100px">
@@ -123,7 +124,7 @@
                                  <div class="sliderCont ">
 
 
-                                        <div class="post " data-aos="fade-left" data-aos-duration="1000">
+                                        <div class="post " data-aos="fade-left" data-aos-delay="0">
                                                 <div style="width:100%; height:100%; background-color:#B9B9B9; padding:10rem; display: flex; justify-content: center; align-items: center; flex-direction: column;">
                                                                 <a href="#"><img src="images/createBtn.svg" width="100px" ></a>
                                                                 <h4 style="color:#928F8F; font-size:2rem;text-align: center; width:12rem;"> Post a News or Event</h4>
@@ -134,7 +135,7 @@
 
 
                                          
-                                         <div class="post hoverable" data-aos="fade-left" data-aos-duration="1000">
+                                         <div class="post hoverable" data-aos="fade-left" data-aos-delay="500">
                                                  <img src="images/delete_24px.svg" width="10px" class="deleteBtn">
                                                  <img class="img" src="images/post_img.jpg" width="100%" >
                                                  <div class="desp">
@@ -146,7 +147,7 @@
                                                  </div>
                                          </div>
                                          
-                                         <div class="post hoverable" data-aos="fade-left" data-aos-duration="1500">
+                                         <div class="post hoverable" data-aos="fade-left" data-aos-delay="1000">
                                          <img src="images/delete_24px.svg" width="10px" class="deleteBtn">
                                                  <img class="img" src="images/post_img.jpg" width="100%" >
                                                  <div class="desp">
@@ -158,7 +159,7 @@
                                                  </div>
                                          </div>
                                          
-                                         <div class="post hoverable" data-aos="fade-left" data-aos-duration="2000">
+                                         <div class="post hoverable" data-aos="fade-left" data-aos-delay="1500">
                                          <img src="images/delete_24px.svg" width="10px" class="deleteBtn">
                                                  <img class="img" src="images/post_img.jpg" width="100%" >
                                                  <div class="desp">
@@ -170,7 +171,7 @@
                                                  </div>
                                          </div>
                                          
-                                         <div class="post hoverable"  data-aos="fade-left" data-aos-duration="2500">
+                                         <div class="post hoverable"  data-aos="fade-left" data-aos-delay="2000">
                                          <img src="images/delete_24px.svg" width="10px" class="deleteBtn">
                                                  <img class="img" src="images/post_img.jpg" width="100%" >
                                                  <div class="desp">
@@ -182,7 +183,7 @@
                                                  </div>
                                          </div>
                                          
-                                         <div class="post hoverable" data-aos="fade-left" data-aos-duration="3000">
+                                         <div class="post hoverable" data-aos="fade-left" data-aos-delay="2500">
                                          <img src="images/delete_24px.svg" width="10px" class="deleteBtn">
                                                  <img class="img" src="images/post_img.jpg" width="100%" >
                                                  <div class="desp">
@@ -194,7 +195,7 @@
                                                  </div>
                                          </div>
                                          
-                                         <div class="post hoverable" data-aos="fade-left" data-aos-duration="3500">
+                                         <div class="post hoverable" data-aos="fade-left" data-aos-delay="3000">
                                          <img src="images/delete_24px.svg" width="10px" class="deleteBtn">
                                                  <img class="img" src="images/post_img.jpg" width="100%" >
                                                  <div class="desp">
@@ -206,7 +207,7 @@
                                                  </div>
                                          </div>
                                          
-                                         <div class="post hoverable" >
+                                         <div class="post hoverable" data-aos="fade-left" data-aos-delay="3500">
                                          <img src="images/delete_24px.svg" width="10px" class="deleteBtn">
                                                  <img class="img" src="images/post_img.jpg" width="100%" >
                                                  
@@ -219,7 +220,7 @@
                                                  </div>
                                          </div>
                                          
-                                         <div class="post hoverable" >
+                                         <div class="post hoverable" class="post hoverable" data-aos="fade-left" data-aos-delay="3500">
                                          <img src="images/delete_24px.svg" width="10px" class="deleteBtn">
                                                  <img class="img" src="images/post_img.jpg" width="100%" >
                                                  <div class="desp">
@@ -261,32 +262,32 @@
 
         <div class="sec"  style="height:120vh; width:100vw; margin-top:8rem; background-color: #A8B66E; border:none;" id="section4" >
                         <div class="col s12" style="padding: 5rem 0rem 2rem; ">
-                                        <h2 class="secTitle" style="margin:5rem 0rem 0rem 0rem; padding:0rem; text-align: center;" id="section5">What we do</h2>
+                                        <h2 class="secTitle" style="margin:5rem 0rem 0rem 0rem; padding:0rem; text-align: center; color:white;" id="section5">What we do</h2>
                                 </div>
                         <ul class="collapsible expandable" style="width:50vw; margin:0 auto; border:none;">
-                                        <li class="active" data-aos="fade-in" data-aos-duration="0">
+                                        <li class="active" data-aos="fade-in" data-aos-delay="100">
                                                 <div class="collapsible-header" style="background-color:#434831; border:none; color:white;"><i class="material-icons"><img src="images/badge1.svg" style="margin-left:-2rem;"></i>Lakes & Rivers</div>
                                                 <div class="collapsible-body"><span>With over two million lakes and rivers and 20 per cent of the world’s freshwater, Canada has an incredible abundance of aquatic natural resources. Canadians share a deep historical connection to freshwater; we rely on it for transportation, for resources, for employment, for food and for recreation — swimming, boating, fishing or simply admiring the scenery of a natural lake or wild river.</span></div>
                                         </li>
-                                        <li class="active" data-aos="fade-in" data-aos-duration="1000">
+                                        <li class="active" data-aos="fade-in" data-aos-delay="200">
                                                 <div class="collapsible-header" style="background-color:#434831; border:none; color:white;"><i class="material-icons"><img src="images/badge2.svg" style="margin-left:-2rem;"></i>Endangered species & biodiversity </div>
                                                 <div class="collapsible-body"><span>Canada is fortunate to be home to thousands of wildlife species and the vast areas of land and water on which they depend. Unfortunately, many of these species are in decline, due in part to human activities. There are currently more than 700 species of plants and animals at risk of being lost from Canada. There isn’t any one culprit, but rather a combination of factors that are leading to species decline.</span></div>
                                         </li>
-                                        <li class="active" data-aos="fade-in" data-aos-duration="1000">
+                                        <li class="active" data-aos="fade-in" data-aos-delay="300">
                                                 <div class="collapsible-header" style="background-color:#434831; border:none; color:white;"><i class="material-icons"><img src="images/badge3.svg" style="margin-left:-2rem;"></i>Fields & Forests</div>
                                                 <div class="collapsible-body"><span>Canadian forests and fields have an important role to play in supporting our communities, our economy and our wildlife. Loss and alteration of habitat, toxic pollutants such the widespread use of pesticides and herbicides and climate change threaten Canada’s wildlife in our forests and farmlands.</span></div>
                                         </li>
-                                        <li class="active" data-aos="fade-in" data-aos-duration="2000">
+                                        <li class="active" data-aos="fade-in" ddata-aos-delay="400">
                                                 <div class="collapsible-header" style="background-color:#434831; border:none; color:white;"><i class="material-icons"><img src="images/badge4.svg" style="margin-left:-2rem;"></i>Coasts & Oceans</div>
                                                 <div class="collapsible-body"><span>Canada is the second largest country in the world; the land area of our country is almost 10 million Km2. What is often forgotten is that our country also includes more than seven million Km2 of ocean and more than 200,000 Km of coastline!
 
                                                                 We utilize the ocean for many purposes</span></div>
                                         </li>
-                                        <li class="active" data-aos="fade-in" data-aos-duration="2500">
+                                        <li class="active" data-aos="fade-in" data-aos-delay="500">
                                                         <div class="collapsible-header" style="background-color:#434831; border:none; color:white;"><i class="material-icons"><img src="images/badge5.svg" style="margin-left:-2rem;"></i>Education</div>
                                                         <div class="collapsible-body"><span>CWF is dedicated to ensuring that our wildlife and natural spaces remain a treasured part of our country. The challenge is inspiring that same conservation ethic in new generations of Canadians to ensure they develop their own passion for wildlife conservation. At CWF, we develop education programs to help connect Canadians to nature. We encourage Canadians of all ages to participate in individual conservation actions and to experience, enjoy and value nature. In an ever-increasing urban society, the greatest challenge is developing those connections to our natural world. Education and experiential learning are key pillars in programming designed to meet this challenge.</span></div>
                                                 </li>
-                                        <li class="active" data-aos="fade-in" data-aos-duration="3000">
+                                        <li class="active" data-aos="fade-in" data-aos-duration="600">
                                                 <div class="collapsible-header" style="background-color:#434831; border:none; color:white;"><i class="material-icons"><img src="images/badge6.svg" style="margin-left:-2rem;"></i>Connect with Nature</div>
                                                 <div class="collapsible-body"><span>Canadians are spending most of their lives indoors – reports indicate as much as 90 per cent of our time is spent inside. Both doctors and scientists confirm that a lack of exposure to nature actually contributes to human illness. Our disconnection from nearby nature is a prime reason for the declining state of human health, both mental and physical.</span></div>
                                         </li>
@@ -435,12 +436,12 @@
                         <img src="images/logo.png" width="60%">
                 </div>
                 <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Links</h5>
+                <h5 class="white-text">External Websites</h5>
                 <ul>
-                        <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                        <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                        <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                        <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">www.WHO.com</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">www.Natgeo.ca</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">www.jjbean.ca</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">www.jackyleeinfo.ca</a></li>
                 </ul>
                 </div>
                 </div>
@@ -448,23 +449,39 @@
                 <div class="footer-copyright">
                 <div class="container">
                         © 2020 Canadian Wildlife Federation. All Rights Reserved.
-                <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+                <a class="grey-text text-lighten-4 right" href="#!">www.instagram/csf.com</a>
                 </div>
                 </div>
         </footer>
 
+        <script src="laxxx/lib/lax.min.js" ></script>
         <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-    <script type="text/javascript" src="js/script.js"></script>
-    <script type="text/javascript" src="js/materialize.min.js"></script>
-    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-    <script src="<?php echo $url?>/js/php.js"></script>
+        <script type="text/javascript" src="js/script.js"></script>
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+        <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+        <script src="<?php echo $url?>/js/php.js"></script>
 
+        <script>
+                window.onload = function() {
+                lax.setup() // init
 
+                const updateLax = () => {
+                        lax.update(window.scrollY)
+                        window.requestAnimationFrame(updateLax)
+                }
+
+                window.requestAnimationFrame(updateLax)
+}
+        </script>
 
                 <script>
+      
+
                 AOS.init({
                         duration: 1000,
                 });
+
+                
                 </script>
   </body>
 </html>
